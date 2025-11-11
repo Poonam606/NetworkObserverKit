77bb0b058999a7fd98ffd6605072c4a32be56c9a
Poonam606 <poonamyadav464@gmail.com>
2025-11-11 17:54:49 +0530

    Initial commit:NetworkObserveKit

---
+++ Sources/NetworkObserverKit/NetWorkObserver.swift
@@ -0,0 +1,41 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+import SwiftUI
+import Network
+
+@MainActor
+@Observable
+class NetWorkObserver {
+    private var networkMonitor = NWPathMonitor()
+    private var workerQueue = DispatchQueue(label: "NetworkObserver")
+    var isConnected = false
+    init() {
+        networkMonitor.pathUpdateHandler = { [weak self] path in
+            DispatchQueue.main.async {
+                self?.isConnected = path.status == .satisfied
+          }
+        }
+        networkMonitor.start(queue: workerQueue)
+    }
+}
+
+// ✅ Environment key definition
+@MainActor
+private struct NetworkObserverKey: @MainActor EnvironmentKey {
+    static let defaultValue = NetWorkObserver()
+}
+
+@MainActor
+extension EnvironmentValues {
+    var network: NetWorkObserver {
+        get { self[NetworkObserverKey.self] }
+        set { self[NetworkObserverKey.self] = newValue }
+    }
+}
+
+// ✅ View extension
+extension View {
+    func listenToNetwork() -> some View {
+        environment(\.network, NetWorkObserver())
+    }
+}
